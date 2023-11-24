e8e0b3086d11683fe871d6d354c3eab17665566f
mark4802 <maje0011@stud.kea.dk>
2023-11-24 14:57:59 +0100

    for en sikkerheds skyld

---
+++ src/main/java/SwimmingTimeComparator.java
@@ -0,0 +1,16 @@
+package comparator;
+
+<<<<<<< HEAD
+import member.Member;
+
+=======
+        >>>>>>> MarkDevelopmentArea
+import java.util.Comparator;
+
+public class SwimmingTimeComparator implements Comparator<Member> {
+
+    @Override
+    public int compare(Member m1, Member m2){
+        return m1.getSwimmingTime().compareTo(m2.getSwimmingTime);
+    }
+}
